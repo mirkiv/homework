c2139dd8188dffbdb776983eff31e9472f64258b
mirkiv <mirkamolmizraahmedov@gmail.com>
2024-10-10 21:28:32 +0500

    sss

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let div = document.querySelector("#greeting");
 function showGreeting() {
-	document.getElementById("greeting").innerHTML += "Привет22 привет<br>";
+	document.getElementById("greeting").innerHTML += "Привет222 привет<br>";
 	setTimeout(showGreeting, 2000);
 }
 
